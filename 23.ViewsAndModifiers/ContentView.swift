2e6508121efddd61c981d2f6640b1fbb63b12357
Valentin <latronixo@gmail.com>
2025-05-16 00:58:03 +0300

    Initial Commit

---
+++ 23.ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  23.ViewsAndModifiers
+//
+//  Created by Валентин on 16.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 23.ViewsAndModifiers/_3_ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_ViewsAndModifiersApp.swift
+//  23.ViewsAndModifiers
+//
+//  Created by Валентин on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _3_ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
