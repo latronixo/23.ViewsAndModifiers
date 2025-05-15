fea8e352957a9dc7676f976432312043e2d7cdfd
Valentin <latronixo@gmail.com>
2025-05-16 01:16:09 +0300

    23.3/10 Порядок модификаторов имеет смысл

--- 23.ViewsAndModifiers/ContentView.swift
+++ 23.ViewsAndModifiers/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.red)
     }
 }
 
